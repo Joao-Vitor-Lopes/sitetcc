65e9e00a304db4c13ff04eb65a181e0c831068c8
Joao-Vitor-Lopes <joao.lopes200@etec.sp.gov.br>
2023-11-23 11:51:02 -0300

    site

--- computer.js
+++ computer.js
@@ -2,6 +2,31 @@ export default class Computer{
     constructor() {}
 
     play(spaces, boxes, level) {
-        console.log(spaces, boxes, level);
+        switch (level) {
+
+            case 1:
+                this.playInEasyLevel(spaces, boxes);
+                break;
+            case 2:
+                
+            default:
+        }
+    }
+
+    playInEasyLevel(spaces, boxes) {
+        boxes[this.getRandomAvailableSpace(spaces)].click()
+        
+    }
+
+    getRandomAvailableSpace(spaces){
+        const availableSpaces = (() => {
+            const aux = [];
+            for(let i = 0; i < spaces.length; i++){
+                if(!spaces[i]) aux.push(i);
+            }
+            return aux;
+        })();
+    
+       return availableSpaces[Math.floor(Math.random() * availableSpaces.length)];
     }
 }
--- scripts.js
+++ scripts.js
@@ -2,9 +2,10 @@ import Computer from "./computer.js";
 
 const playerText = document.getElementById("playerText");
 const restartBtn = document.getElementById("restartBtn");
-const boxes = Array.from (document.getElementsByClassName("box"));
+const boxes = Array.from(document.getElementsByClassName("box"));
 const winningIndicator = getComputedStyle(document.body).getPropertyValue("--winning-blocks"
 );
+
     const xPointsText = document.getElementById("x-points");
     const oPointsText = document.getElementById("o-points");
     const levelSelect = document.getElementById("difficulty-select")
@@ -17,6 +18,7 @@ const X_TEXT = "X";
 let currentPlayer = X_TEXT;
 let spaces = Array(9).fill(null);
 let level = 1;
+const computer = new Computer();
 
 levelSelect.addEventListener('change', (e) =>{
     
@@ -53,8 +55,8 @@ function boxClicked(e) {
         }
 
         currentPlayer = currentPlayer === X_TEXT ? O_TEXT : X_TEXT;
-        if(currentPlayer === O_TEXT){
-       Computer.play(spaces, boxes, level);
+        if(currentPlayer === O_TEXT && getRemainingSpaces().length){
+       computer.play(spaces, boxes, level);
         }
     }
 }
@@ -83,18 +85,24 @@ function playerHasWon(){
     return false;
 }
 
-function isDraw(){
-      for(let i = 0; i < spaces.length; i++){
-        if(spaces[i] === null) return false;
-      }
-      return true
-}
+
 
 function addPointsToWinner(){
     if(currentPlayer === X_TEXT)
     xPointsText.innerText = Number(xPointsText.innerText) + 1;
 else    oPointsText.innerText = Number(xPointsText.innerText) + 1;
 
+}
+
+function isDraw(){
+    for(let i = 0; i < spaces.length; i++){
+      if(spaces[i] === null) return false;
+    }
+    return true
+}
+
+function getRemainingSpaces(){
+    return spaces.filter((space) => !space);
 }
 
 restartBtn.addEventListener("click", restart);

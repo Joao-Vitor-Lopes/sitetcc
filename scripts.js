379fbe9ec9ca0b50ec8cd5f4fdee7b71f954cae7
Joao-Vitor-Lopes <joao.lopes200@etec.sp.gov.br>
2023-11-24 17:48:28 -0300

    site

--- scripts.js
+++ scripts.js
@@ -1,6 +1,19 @@
 class Computer{
     constructor() {}
 
+
+ winningCombos = [
+
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+         
+    ];
     play(spaces, boxes, level) {
         switch (level) {
 
@@ -17,9 +30,14 @@ class Computer{
         boxes[this.getRandomAvailableSpace(spaces)].click();
     }
 
-    playInMiddleLevel(spaces, boxes){
+    playInMiddleLevel(spaces, boxes) {
         if(this.getRandomAvailableSpace(spaces).length === 9){
             boxes[this.getRandomAvailableSpace(spaces)].click();
+        } else if(this.getRandomAvailableSpace(spaces).length === 8){
+            if (!spaces[4]) return boxes[4].click();
+            boxes[this.getRandomAvailableSpace(spaces)].click();
+        } else if(this.getSpaceToWin(spaces)){
+           boxes[this.getSpaceToWin(spaces)].click();
         }
     }
 
@@ -34,7 +52,24 @@ class Computer{
     
        return availableSpaces[Math.floor(Math.random() * availableSpaces.length)];
     }
-}
+    getRemainingSpaces(spaces) {
+        return spaces.filter((space) => !space)
+    }
+
+    getSpaceToWin(spaces){
+        for(const combo of this.winningCombos){
+            const [a, b, c] = combo;
+    
+            if(spaces[a] === "O" && spaces[b] === "O" && !spaces[c]) return c;
+            else if (spaces[a] === "O" && spaces[c] === "O" && !spaces[b]) return b;
+            else if(spaces[b] === "O" && spaces[c] === "O" && !spaces[a]) return a;
+            }
+        return null;
+    }
+  }
+
+
+
 
 const playerText = document.getElementById("playerText");
 const restartBtn = document.getElementById("restartBtn");
@@ -153,6 +188,12 @@ function restart(){
 });
 
     playerText.innerText = "JOGO DA VELHA"
+
+    currentPlayer = currentPlayer === X_TEXT ? O_TEXT : X_TEXT;
+    if(currentPlayer === O_TEXT) {
+        computer.play(spaces, boxes, level);
+    }
 }
 
 startGame();
+
